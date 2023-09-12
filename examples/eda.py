ceb6804606ce081f072e129286a267b3afadf599
rainyrfeng <rainyrfeng@tencent.com>
2023-09-12 22:43:11 +0800

    fix feature

--- examples/eda.py
+++ examples/eda.py
@@ -2,13 +2,13 @@
 
 import asyncio
 
-from metagpt.actions.data_process import DataAnalyse
+from metagpt.actions.data_process import DataProcess
 
 
 async def main():
     file_path = "../tests/data/data_for_test.csv"
     context = "Analyze loans for different age groups?"
-    role = DataAnalyse()
+    role = DataProcess()
     await role.run(context, file_path)
 
 
